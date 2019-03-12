657690b76eadd50546ffc7d88bd9bce3e3f64fae
Emily DeWitt <emilykdewitt@gmail.com>
2019-03-11 19:44:25 -0500

    Added piemaker function

--- main.js
+++ main.js
@@ -1,43 +1,81 @@
-const pies = [
-    {
-        name: "Dutch Apple Pie",
-        ingredients: 'apples,sugar,butter,nutmeg,dutch people',
-        bakeTemp: 5000,
-        drinkPairing: 'Earl Grey',
-        imageUrl: 'https://images-gmi-pmc.edge-generalmills.com/b6a2a4e7-73f5-4aec-9bb6-f2b5054d65e6.jpg',
-        instructor: 'Zoe',
-        iceCreamPairing: 'Vanilla'
-    },
-    {
-        name: 'Berry Pie',
-        ingredients: 'berries',
-        bakeTemp: 5000,
-        drinkPairing: 'wine',
-        imageUrl: 'https://d1doqjmisr497k.cloudfront.net/-/media/mccormick-us/recipes/mccormick/m/800/mixed-berry-pie.ashx?vd=20180710T032825Z&hash=43EA41794B894116CBFA583CB5EE13C2E062F46E',
-        instructor: 'Zoe',
-        iceCream: 'Vanilla'
-    },
-    {
-        name: 'Test1',
-    },
-    {
-        name: 'Test2',
-    },
-    {
-        name: 'Test3',
-    }
-];
-
 const printToDom = (divId, textToPrint) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = textToPrint;
 };
 
-const pieMaker = () => {
+const pies = [
+    {
+        name: 'Dutch Apple Pie',
+        ingredients: 'apples,sugar,butter,nutmeg,dutch people',
+        bakeTemp: 5000,
+        drinkPairing: 'Earl Grey Tea',
+        imageUrl: 'https://search.chow.com/thumbnail/800/0/www.chowstatic.com/assets/2013/09/30859_Recipeimage_620x413_dutch_apple_pie.jpg',
+        instructor: 'Zoe',
+        iceCream: 'Vanilla',
+      },
+      {
+        name: 'Berry Pie',
+        ingredients: 'berries',
+        bakeTemp: 400,
+        drinkPairing: 'wine',
+        imageUrl: 'https://tastesbetterfromscratch.com/wp-content/uploads/2015/11/Triple_Berry_Pie8.jpg',
+        instructor: 'Zoe',
+        iceCream: 'banana',
+      },
+      {
+        name: 'Pumpkin Pie',
+        ingredients: 'pumpkins, nutmeg, cinnamon, graham crackers, pilgrims',
+        bakeTemp: 42,
+        drinkPairing: 'egg nog',
+        imageUrl: 'https://cf-images.us-east-1.prod.boltdns.net/v1/static/1033249144001/22a0c25d-2fee-415c-a4e7-91d008e276bb/a904f3e2-3cd9-40d6-ace9-f8dbd2d616cd/1280x720/match/image.jpg',
+        instructor: 'Michael',
+        iceCream: 'Vanilla',
+      },
+      {
+        name: 'Shoo Fly Pie',
+        ingredients: 'Molasses, nutmeg, cinnamon, butter, graham cracker ',
+        bakeTemp: 1234,
+        drinkPairing: 'Apple Cider',
+        imageUrl: 'https://static01.nyt.com/images/2016/09/27/dining/27COOKING-SHOOFLY-PIE2/27COOKING-SHOOFLY-PIE2-articleLarge.jpg',
+        instructor: 'Michael',
+        iceCream: 'Coffee',
+      },
+      {
+        name: 'Pecan Pie',
+        ingredients: 'Pecans, sugar, butter, flour',
+        bakeTemp: 5000,
+        drinkPairing: 'Milk',
+        imageURL: 'https://cookiesandcups.com/wp-content/uploads/2018/10/pecanpie-3.jpg',
+        instructor: 'Saul',
+        iceCream: 'Vanilla',
+      },
+      {
+        name:'Keylime Pie',
+        ingredients: 'lemons, sugar, butter, flour',
+        bakeTemp: 5000,
+        drinkPairing: 'Water',
+        imageURL: 'https://www.williams-sonoma.com/wsimgs/rk/images/dp/recipe/201851/0020/img38l.jpg',
+        instructor: 'Saul',
+        iceCream: 'none',
+      },
+    ];
+
+const pieBuilder = () => {
     let domString = '';
-    for (i = 0; i < pies.length; i++) {
-        domString += `<div class="yum">`;
-        domString +=    `${pies[i].name}`
-        domString += `</div>`
-    };
+    pies.forEach((pie) => {
+        domString += `<h3>${pie.name}</h3>`;
+        domString += `<p>Ingredients: ${pie.ingredients}</p>`;
+        domString += `<p>Bake Temperature: ${pie.bakeTemp}</p>`;
+        domString += `<p>Drink pairing: ${pie.drinkPairing}</p>`;
+        domString += `<p><img src=${pie.imageUrl}></p>`;
+        domString += `<p>Instructor: ${pie.instructor}</p>`;
+        domString += `<p>Ice Cream Pairing: ${pie.iceCreamPairing}</p>`;
+    })
+    printToDom('pieList', domString);
 };
+
+const init = () => {
+    pieBuilder();
+};
+
+init();
